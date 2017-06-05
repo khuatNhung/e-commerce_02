4f8ec6f3dc6aa72df5f83c671b86f80edf89043d
nhungkt <dreamhigh.kn95@gmail.com>
2017-06-05 10:49:11 +0700

    Init layout admin

---
+++ resources/lang/en/admin/label.php
@@ -0,0 +1,24 @@
+<?php 
+
+return [
+	/*
+    |--------------------------------------------------------------------------
+    | Label form
+    |--------------------------------------------------------------------------
+    |
+    */
+	'categories_index' => 'List Categories',
+	'name' => 'Category name',
+	'parent_category' => 'Parent category',
+	'url_category' => 'Category link',
+	'description' => 'Description',
+	'cancel' => 'Cancel',
+	'save' => 'Save',
+	/*
+    |--------------------------------------------------------------------------
+    | Label view create category
+    |--------------------------------------------------------------------------
+    |
+    */
+	'categories_create' => 'Create category',
+];
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    {{ Html::style(mix('css/admin.min.css')) }}
+    
+  </head>
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            </div>
+            <div class="clearfix"></div>
+            <!-- menu profile quick info -->
+            @include('admin.layouts.section.profile')
+            <!-- /menu profile quick info -->
+            <br />
+            <!-- sidebar menu -->
+            @include('admin.layouts.section.sidebarmenu')
+            <!-- /sidebar menu -->
+            <!-- /menu footer buttons -->
+            @include('admin.layouts.section.sidebarfooter')
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+        <!-- top navigation -->
+        @include('admin.layouts.section.topnav')
+        <!-- /top navigation -->
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>@yield('heading')</h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>@yield('heading')</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                  @yield('content')
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+        <!-- footer content -->
+        @include('admin.layouts.section.footer')
+        <!-- /footer content -->
+      </div>
+    </div>
+    {{ Html::script(mix('js/admin.min.js')) }}
+    @stack('scripts')
+    
+  </body>
+</html>
